b65c76b805bcb2a7c2319625e1cb81e5ad230eb7
Andrei Zharov <andrei.zharov@here.com>
2014-10-24 15:51:16 -0400

    update acceptance test sample

--- tests/e2e/test.spec.js
+++ tests/e2e/test.spec.js
@@ -6,19 +6,19 @@ chai.should();
 
 describe('Home Page smoke test', function() {
 
-  before(function() {
+    before(function() {
 
-  });
+    });
 
-  after(function() {
-    return this.browser.quit();
-  });
+    after(function() {
+        return this.browser.quit();
+    });
 
-  beforeEach(function() {
-    return this.browser.get("http://localhost:3000");
-  });
+    beforeEach(function() {
+        return this.browser.get("http://localhost:3000");
+    });
 
-  it('should have basic page', function() {
-    return this.browser.title().should.become("Andrei Zharov :: Personal site");
-  });
+    it('should have basic title', function() {
+        return this.browser.title().should.become("Andrei Zharov :: Personal site");
+    });
 });
